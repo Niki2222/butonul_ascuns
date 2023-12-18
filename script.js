93c3ca6710445d9618aa9fb91f8bf11b3732e5dd
Niki2222 <89404731+Niki2222@users.noreply.github.com>
2023-12-18 16:12:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let winningNo = Math.floor(Math.random() * 3) + 1;
+console.log(winningNo);
+
+function checkWinOrLoose() {
+    let buttonNo = parseInt(document.activeElement.innerHTML);
+    if (buttonNo === winningNo) {
+        alert(`YOU WON!!! Button ${buttonNo} is a winning button!`);
+    } else {
+        alert(`Button ${buttonNo} is a loosing button. Try again!`);
+    }
+}
+
+function generateButtons() {
+    let buttonsNo = parseInt(document.getElementById('buttonsNumber').value);
+    winningNo = Math.floor(Math.random() * buttonsNo) + 1;
+    console.log(winningNo);
+    for (let i = 1; i <= buttonsNo; ++i) {
+        document.getElementById('showButtons').innerHTML += `
+        <button type="button" class="btn btn-primary" onclick="checkWinOrLoose()">${i}</button>
+        `
+    }
+}
